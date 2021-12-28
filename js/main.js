fa48a46444da4777fa7d3a6db10ad33e4abffd2d
stephie <reynolds.stephie@gmail.com>
2021-12-28 17:08:05 -0300

    first js

---
+++ js/main.js
@@ -0,0 +1,25 @@
+// Stream
+var fruit = 'Pera';
+
+//Numbers
+const NumberCien = 100;
+let number = 20;
+
+//Boolean
+let EstoyDormido = false;
+
+let resultado = number*NumberCien;
+
+console.log (resultado);
+
+let HorasExtras = true;
+
+if(HorasExtras){
+    alert('el codigo se ejecuta')
+    console.log ('esta falso')}
+
+let UserName = prompt ("Ingrese su usuario");
+
+if UserName ="" 
+
+// . + CTRL + SPACE short cut 
